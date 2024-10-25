6631e2fd41e635bd9e3bf8dbcb1b4181d39d5152
takeki-99 <takekidanmkk@gmail.com>
2024-10-25 23:54:41 +0900

    ZHに対応

--- src/pdf_create/creater.py
+++ src/pdf_create/creater.py
@@ -4,27 +4,34 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from translate.translator import translate_text
 
-# idoc をもとに, output_file_name に PDF を作成する
-def create_pdf(i_doc: pymupdf.Document, output_file_name: str):
+def get_font_name(lang: str) -> str:
+    if lang == "JA":
+        return "japan-s"
+    if lang == "ZH":
+        return "china-ts"
+    return "Helvetica"
+
+# input_file_name をもとに, output_file_name に PDF を作成する
+def create_pdf(input_file_name: str, output_file_name: str, lang: str):
+    i_doc = pymupdf.Document(input_file_name)
     o_doc = pymupdf.Document()
     for i_page in i_doc:
         new_page = o_doc.new_page(width=i_page.rect.width, height=i_page.rect.height)
-        for block in i_page.get_text("blocks"):
-            x_0, y_0, x_1, y_1, text, block_num, block_type = block
+        for block in i_page.get_text("blocks", sort=True):
+            x_0, y_0, x_1, _, text, block_num, block_type = block
             if block_type == 1:
-                print(f"image was skipped. block_num: {block[5]}")
+                print(f"image was skipped. block_num: {block_num}")
                 continue
             font_size = 11
-            translated_text = translate_text(text, "JA", x_1 - x_0, font_size)
-            new_page.insert_text(point=(block[0], block[1]),text=translated_text, 
-                                 fontname="japan", fontsize=font_size)
+            translated_text = translate_text(text, lang, x_1 - x_0, font_size)
+            font_name = get_font_name(lang)
+            new_page.insert_text(
+                point=(x_0, y_0),
+                text=translated_text,
+                fontname=font_name,
+                fontsize=font_size)
     # save
     o_doc.save(output_file_name)
     o_doc.close()
     i_doc.close()
-
-if __name__ == "__main__":
-    INPUT_FILE_NAME = "sample/input/sample.pdf"
-    OUTPUT_FILE_NAME = "sample/output/sample.pdf"
-    i_doc = pymupdf.Document(INPUT_FILE_NAME)
-    create_pdf(i_doc, OUTPUT_FILE_NAME)
+    print(f"output file was saved as {output_file_name}", end="\n\n")
