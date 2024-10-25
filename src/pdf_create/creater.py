9c704d311966ea898d5b3ee92b4d0fb8070b9c6c
takeki-99 <takekidanmkk@gmail.com>
2024-10-25 22:47:48 +0900

    add newline

--- src/pdf_create/creater.py
+++ src/pdf_create/creater.py
@@ -10,15 +10,14 @@ def create_pdf(i_doc: pymupdf.Document, output_file_name: str):
     for i_page in i_doc:
         new_page = o_doc.new_page(width=i_page.rect.width, height=i_page.rect.height)
         for block in i_page.get_text("blocks"):
-            if block[6] == 1:
+            x_0, y_0, x_1, y_1, text, block_num, block_type = block
+            if block_type == 1:
                 print(f"image was skipped. block_num: {block[5]}")
                 continue
-            translated_text = translate_text(block[4], "JA")
-            print(type(translated_text))
-            print(f"translated: {block[4]} -> {translated_text}")
-            print(font_path)
-            new_page.insert_text(point=(block[0], block[1]),text=translated_text, fontname="japan")
-        break
+            font_size = 11
+            translated_text = translate_text(text, "JA", x_1 - x_0, font_size)
+            new_page.insert_text(point=(block[0], block[1]),text=translated_text, 
+                                 fontname="japan", fontsize=font_size)
     # save
     o_doc.save(output_file_name)
     o_doc.close()
--- src/translate/translator.py
+++ src/translate/translator.py
@@ -2,17 +2,31 @@ from dotenv import load_dotenv
 import os
 import deepl
 
+def add_newline(text: str, width: int, font_size: int) -> str:
+    # calculate char_num_per_line
+    char_num_per_line = int(width / font_size)
+    # every line has char_num_per_line characters
+    lines = [text[i:i+char_num_per_line] for i in range(0, len(text), char_num_per_line)]
+    return "\n".join(lines)
+
 # tlanslate text to target language
-def translate_text(text: str, lang: str) -> str:
-    return "あいうえお"
+# def translate_text(text: str, lang: str) -> str:
+def translate_text(text: str, lang: str, text_area_width: int, font_size: int) -> str:
+    # make translator
     load_dotenv()
     api_key = os.getenv("DEEPL_API_KEY")
     if api_key is None:
         raise ValueError("DEEPL_API_KEY is not set")
     translator = deepl.Translator(api_key)
+
     # remove newline
     text = text.replace("\n", " ")
     result = translator.translate_text(text, target_lang=lang)
-    print(f"translated: {text} -> {result.text}")
-    return result.text
+    
+    # output result
+    print(f"{text} -> {result.text}")
+
+    # add newline
+    translated_text = add_newline(result.text, text_area_width, font_size)
+    return translated_text
     
